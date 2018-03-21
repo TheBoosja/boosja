f51aa47ea0950e62a5bfa9714783f219b8524da5
unknown <mathias.iversen487@gmail.com>
2018-03-21 15:56:25 +0100

    Styled Form Elements

---
+++ src/components/styled/form.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+	display: grid;
+	grid-template-columns: 50vw;
+	align-content: center;
+	justify-content: center;
+`;
+
+export const Input = styled.input`
+	background-color: ${({ theme }) => theme.bgLight};
+	border: none;
+	color: inherit;
+	font-family: inherit;
+	font-size: 2rem;
+	padding: 1rem 2rem;
+	width: 100%;
+
+	&:focus {
+		outline: none;
+	}
+`;
+
+export const Error = styled.span`
+	display: block;
+	color: red;
+	font-size: 1.2rem;
+	margin: 1rem 2rem;
+`;
