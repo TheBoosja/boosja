9f34f242c4949006ee91271fe574fa51daab42de
unknown <mathias.iversen487@gmail.com>
2018-03-24 00:35:14 +0100

    Refactor of filter check

--- src/components/base/Navbar.js
+++ src/components/base/Navbar.js
@@ -10,20 +10,26 @@ export const Navbar = ({ isAuthenticated }) => {
 		{ to: '/profile', label: 'Profile', auth: true },
 		{ to: '/search', label: 'Search' },
 		{ to: '/signout', label: 'Sign Out', auth: true },
-		{ to: '/auth', label: 'Sign In', auth: false }
+		{ to: '/signin', label: 'Sign In', auth: false },
+		{ to: '/register', label: 'Register', auth: false }
 	];
 
-	const computedList = (auth) => {
-		const filteredItems = _.filter(navItems, 
-			item => _.isUndefined(item.auth) ? true : item.auth === auth);
+	const filteredItems = _.filter(navItems, item => {
+		if (_.isUndefined(item.auth)) {
+			return true;
+		}
+		
+		const authed = _.isUndefined(isAuthenticated) ? false : isAuthenticated;
 
-		return _.map(filteredItems, ({ exact, to, label }, i) => 
-			<NavItem exact={exact || false} to={to} key={i}>{label}</NavItem>);
-	};
+		return item.auth === authed;
+	});
+
+	const navLinks = _.map(filteredItems, ({ exact, to, label }, i) => 
+		<NavItem exact={exact || false} to={to} key={i}>{label}</NavItem>);
 
 	return (
 		<Nav>
-			{computedList(isAuthenticated)}
+			{navLinks}
 		</Nav>
 	);
 };
