e175146078a1b645f8ae7826b05d35f6a63993a1
unknown <mathias.iversen487@gmail.com>
2018-03-24 00:31:33 +0100

    Added Title

--- src/components/styled/typography.js
+++ src/components/styled/typography.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
+export const Heading = styled.h1`
 	font-size: 5rem;
 	font-weight: 700;
 	color: ${({ theme }) => theme.textColor};
+	margin-bottom: 2rem;
+`;
+
+export const Title = styled.h2`
+	font-size: 3.5rem;
+	color: ${({ theme }) => theme.textColor};
+	margin-bottom: 2rem;
+	text-align: center;
 `;
