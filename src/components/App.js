c174b3896cd8a192525e7e73ddaafb3bf18b0e04
unknown <mathias.iversen487@gmail.com>
2018-03-24 00:30:26 +0100

    Authentication check from cookie

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from './theme';
+// Auth
+import { auth } from '../actions/firebase';
+import { AUTH_USER } from '../actions/types';
+
 import Navbar from './base/Navbar';
 import Routes from './routes';
 
@@ -10,16 +14,30 @@ const Layout = styled.div`
 	grid-template-columns: 10vw 1fr;
 `;
 
-const App = () => {
-	return (
-		<ThemeProvider theme={theme}>
-			<Layout>
-				<Navbar />
+class App extends Component {
+	componentWillMount() {
+		this.removeListener = auth.onAuthStateChanged(user => {
+			if (user) {
+				this.props.store.dispatch({ type: AUTH_USER });
+			}
+		});
+	}
 
-				<Routes />
-			</Layout>
-		</ThemeProvider>
-	);
+	componentWillUnmount() {
+		this.removeListener();
+	}
+
+	render() {
+		return (
+			<ThemeProvider theme={theme}>
+				<Layout>
+					<Navbar />
+
+					<Routes />
+				</Layout>
+			</ThemeProvider>
+		);
+	}
 };
 
 export default App;
