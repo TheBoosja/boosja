61a1d4a397a51e6bc0f36309cffd860ca3104420
unknown <mathias.iversen487@gmail.com>
2018-03-24 09:38:59 +0100

    Tests auth routes

--- src/components/routes/index.test.js
+++ src/components/routes/index.test.js
@@ -15,7 +15,17 @@ describe('Routes', () => {
 	});
 
 	it('contains a Sign In route', () => {
-		const auth = routes.findWhere(r => r.props().path === '/auth');
-		expect(auth.exists()).toBe(true);
+		const signIn = routes.findWhere(r => r.props().path === '/signin');
+		expect(signIn.exists()).toBe(true);
+	});
+
+	it('contains a Register route', () => {
+		const register = routes.findWhere(r => r.props().path === '/register');
+		expect(register.exists()).toBe(true);
+	});
+
+	it('contains a Sign Out route', () => {
+		const signOut = routes.findWhere(r => r.props().path === '/signout');
+		expect(signOut.exists()).toBe(true);
 	});
 });
