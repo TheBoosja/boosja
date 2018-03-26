d14e1484971e5c0683009e29872c0d746840f272
unknown <mathias.iversen487@gmail.com>
2018-03-26 14:07:07 +0200

    Search types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,6 @@
 export const AUTH_USER = 'AUTH_USER';
 export const UNAUTH_USER = 'UNAUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+export const SEARCH_RESULT = 'SEARCH_RESULT';
+export const EMPTY_RESULT = 'EMPTY_RESULT';
