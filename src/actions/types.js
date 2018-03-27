3b1a9daabd20827dd6436eed8635acd1029d492b
unknown <mathias.iversen487@gmail.com>
2018-03-27 15:42:44 +0200

    Added auth listener type

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,7 @@
 export const AUTH_USER = 'AUTH_USER';
 export const UNAUTH_USER = 'UNAUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
+export const AUTH_REMOVE_LISTENER = 'AUTH_REMOVE_LISTENER';
 
 export const SEARCH_RESULT = 'SEARCH_RESULT';
 export const EMPTY_RESULT = 'EMPTY_RESULT';
