c698cc04fa5c0b7dcbf8d4294e2f8355a6c098cb
unknown <mathias.iversen487@gmail.com>
2018-03-31 09:03:51 +0200

    Added search reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 import { reducer as form } from 'redux-form';
+
 import auth from './auth';
+import search from './search';
 
 const rootReducer = combineReducers({
 	router,
 	form,
-	auth
+	auth,
+	search
 });
 
 export default rootReducer;
