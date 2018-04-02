f43d6293d94665dea6fdd59fe1d358b62e908c46
unknown <mathias.iversen487@gmail.com>
2018-04-02 12:44:20 +0200

    Added styling

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { SearchPage } from 'components/styled';
 
 import SearchBar from './SearchBar';
 import SearchList from './SearchList';
 
 const Search = ({ match, history }) => {
 	return (
-		<div>
+		<SearchPage>
 			<SearchBar {...{ match, history }} />
 			<Route path={`${match.url}/:query/:page?`} component={SearchList} />
-		</div>
+		</SearchPage>
 	);
 };
 
