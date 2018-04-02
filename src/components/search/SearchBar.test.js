90479fd55251c1e7f46ba44007212bbf72567571
unknown <mathias.iversen487@gmail.com>
2018-04-02 12:43:31 +0200

    Validation tests

--- src/components/search/SearchBar.test.js
+++ src/components/search/SearchBar.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { SearchBar } from './SearchBar';
+import { SearchBar, validate } from './SearchBar';
 
 describe('Search Bar', () => {
 	const mockPush = jest.fn();
 	const mockHandleSubmit = jest.fn();
 
-	const props = { 
+	const props = {
 		history: { push: mockPush },
 		match: { url: '/search' },
 		handleSubmit: mockHandleSubmit
@@ -17,8 +17,8 @@ describe('Search Bar', () => {
 		expect(searchBar).toMatchSnapshot();
 	});
 
-	it('contains a Form component', () => {
-		expect(searchBar.find('Form').exists()).toBe(true);
+	it('contains a form component', () => {
+		expect(searchBar.find('form').exists()).toBe(true);
 	});
 
 	it('contains a Field component', () => {
@@ -31,6 +31,16 @@ describe('Search Bar', () => {
 			searchBar.instance().onFormSubmit(query);
 
 			expect(mockPush).toHaveBeenCalledWith(`/search/${query.query}`);
+		});
+	});
+
+	describe('Validation', () => {
+		it('returns error when no query is entered', () => {
+			expect(validate({ query: '' })).toEqual({ query: 'Enter a query' });
+		});
+
+		it('returns no error when query is entered', () => {
+			expect(validate({ query: 'Breaking Bad' })).toEqual({});
 		});
 	});
 });
